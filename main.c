da5ee1f355a80294aca56aa8bb3835a8d0eac01c
Soulaimane  Limane <slimane@e3r8p14.1337.ma>
2025-06-27 16:57:41 +0100

    imik n tbdit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: slimane <slimane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 13:07:59 by slimane           #+#    #+#             */
-/*   Updated: 2025/06/27 12:14:09 by slimane          ###   ########.fr       */
+/*   Updated: 2025/06/27 16:57:20 by slimane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -160,7 +160,7 @@ int	ft_check_die(t_philo **data)
 		if ((get_time() - (*data)[i].last_meal) > (*data)->info->time_to_die)
 		{
 			lock(&(*data)[i].meal, 2);
-				ft_check_dead(&(*data)[i]);
+			ft_check_dead(&(*data)[i]);
 			return (1);
 		}
 		(lock(&(*data)[i].meal, 2), lock(&(*data)[i].count_m, 1));
@@ -183,6 +183,7 @@ void	moniteur(t_philo *data)
 
 	while (1)
 	{
+		usleep(100);
 		lock(&data[0].info->data_lo, 1);
 		if (data->info->philo_is_die == 1)
 		{
@@ -197,8 +198,6 @@ void	moniteur(t_philo *data)
 }
 
 
-
-
 int	should_exit(t_philo *philo)
 {
 
@@ -208,126 +207,116 @@ int	should_exit(t_philo *philo)
 	return (lock(&philo->info->data_lo, 2), 0);
 }
 
-void	*routine(void *data)
+void	take_fork(t_philo *philo)
 {
-	t_philo *philo = (t_philo *)data;
-
-	lock(&philo->meal, 1);
-	philo->last_meal = philo->start;
-	lock(&philo->meal, 2);
-
-	if (philo->id % 2 == 0)
-		ft_usleep(philo->info->time_to_eat / 2, philo);
-
-	while (1)
+	lock(philo->right_fork, 1), lock(&philo->info->print, 1);
+	if (should_exit(philo))
 	{
-		if (should_exit(philo))
-			break;
+		(lock(&philo->info->print, 2), lock(philo->right_fork, 2));
+		return ;
+	}
+	printf("%ld %d has taken right fork\n", get_time() - philo->start, philo->id);
+	lock(&philo->info->print, 2);
+	if (should_exit(philo))
+		return ((void)lock(philo->right_fork, 2));
+	(lock(philo->left_fork, 1), lock(&philo->info->print, 1));
+	if (should_exit(philo))
+	{
+		(lock(&philo->info->print, 2), lock(philo->left_fork, 2));
+		lock(philo->right_fork, 2);
+		return ;
+	}
+	printf("%ld %d has taken left fork\n", get_time() - philo->start, philo->id);
+	if (should_exit(philo))
+	{
+		lock(&philo->info->print, 2), lock(philo->left_fork, 2);
+		lock(philo->right_fork, 2);
+		return ;
+	}
+	lock(&philo->info->print, 2);
+}
 
-		lock(philo->right_fork, 1);
+void	ft_eat(t_philo	*philo)
+{
 		lock(&philo->info->print, 1);
 		if (should_exit(philo))
 		{
-			lock(&philo->info->print, 2);
+			(lock(&philo->info->print, 2), lock(philo->left_fork, 2));
 			lock(philo->right_fork, 2);
-			break;
+			return ;
 		}
-		printf("%ld %d has taken fork\n", get_time() - philo->info->start_time, philo->id);
+		printf("%ld %d is eating\n", get_time() - philo->start, philo->id);
 		lock(&philo->info->print, 2);
-
+		ft_usleep(philo->info->time_to_eat, philo);
 		if (should_exit(philo))
 		{
-			lock(philo->right_fork, 2);
-			break;
+			(lock(philo->left_fork, 2), lock(philo->right_fork, 2));
+			return ;
 		}
-
-		lock(philo->left_fork, 1);
-		lock(&philo->info->print, 1);
-		if (should_exit(philo))
-		{
-			lock(&philo->info->print, 2);
-			lock(philo->left_fork, 2);
-			lock(philo->right_fork, 2);
-			break;
-		}
-		printf("%ld %d has taken fork\n", get_time() - philo->info->start_time, philo->id);
-		if (should_exit(philo))
-		{
-			lock(&philo->info->print, 2);
-			lock(philo->left_fork, 2);
-			lock(philo->right_fork, 2);
-			break;
-		}
-		lock(&philo->info->print, 2);
-
-		lock(&philo->info->print, 1);
-		if (should_exit(philo))
-		{
-			lock(&philo->info->print, 2);
-			lock(philo->left_fork, 2);
-			lock(philo->right_fork, 2);
-			break;
-		}
-		printf("%ld %d is eating\n", get_time() - philo->info->start_time, philo->id);
-		lock(&philo->info->print, 2);
-
 		lock(&philo->meal, 1);
 		philo->last_meal = get_time();
 		lock(&philo->meal, 2);
 		lock(&philo->count_m, 1);
 		philo->count_meals++;
 		lock(&philo->count_m, 2);
-
-		
-		ft_usleep(philo->info->time_to_eat, philo);
-
 		if (should_exit(philo))
-		{
-			lock(philo->left_fork, 2);
-			lock(philo->right_fork, 2);
-			break;
-		}
+			return (lock(philo->left_fork, 2), (void)lock(philo->right_fork, 2));
+		(lock(philo->left_fork, 2), lock(philo->right_fork, 2));
+}
 
-
-		if (should_exit(philo))
-		{
-			lock(philo->left_fork, 2);
-			lock(philo->right_fork, 2);
-			break;
-		}
-
-		lock(philo->left_fork, 2);
-		lock(philo->right_fork, 2);
-
-		lock(&philo->info->print, 1);
-		if (should_exit(philo))
-		{
-			lock(&philo->info->print, 2);
-			break;
-		}
-		printf("%ld %d is sleeping\n", get_time() - philo->info->start_time, philo->id);
+void	ft_sleep(t_philo *philo)
+{
+	lock(&philo->info->print, 1);
+	if (should_exit(philo))
+	{
 		lock(&philo->info->print, 2);
+		return ;
+	}
+	printf("%ld %d is sleeping\n", get_time() - philo->start, philo->id);
+	lock(&philo->info->print, 2);
+	ft_usleep(philo->info->time_to_sleep, philo);
+}
 
-		ft_usleep(philo->info->time_to_sleep, philo);
-
-		if (should_exit(philo))
-			break;
-
-		lock(&philo->info->print, 1);
-		if (should_exit(philo))
-		{
-			lock(&philo->info->print, 2);
-			break;
-		}
-		printf("%ld %d is thinking\n", get_time() - philo->info->start_time, philo->id);
+void	ft_think(t_philo *philo)
+{
+	lock(&philo->info->print, 1);
+	if (should_exit(philo))
+	{
 		lock(&philo->info->print, 2);
+		return ;
+	}
+	printf("%ld %d is thinking\n", get_time() - philo->info->start_time, philo->id);
+	lock(&philo->info->print, 2);
+}
 
+void	*routine(void *data)
+{
+	t_philo *philo;
+
+	philo = (t_philo *)data;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+	lock(&philo->meal, 1);
+	(philo->last_meal = philo->start, lock(&philo->meal, 2));
+	if (philo->id % 2 == 0)
+		ft_usleep(philo->info->time_to_eat / 2, philo);
+	while (1)
+	{
+		if (should_exit(philo))
+			return (NULL);
+		take_fork(philo);
+		if (should_exit(philo))
+			break;
+		ft_eat(philo);
+		if (should_exit(philo))
+			break;
+		ft_sleep(philo);
+		if (should_exit(philo))
+			break;
+		ft_think(philo);
 		if (should_exit(philo))
 			break;
 	}
 	return (NULL);
 }
-
 
 int main(int ac, char **av)
 {
